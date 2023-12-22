c6091db8777367882b72cb983fa31d1096b1eff7
yosnak13 <yoshihisa.nakai@ft-labo.co.jp>
2023-12-22 15:09:08 +0900

    comment out

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"todo_app/app/models"
-)
-
 func main() {
 
 	/*
@@ -17,18 +12,21 @@ func main() {
 		u.CreateUser()
 	*/
 
-	fmt.Println(models.Db)
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
+	/*
+		fmt.Println(models.Db)
+		u, _ := models.GetUser(1)
+		fmt.Println(u)
 
-	//u.Name = "test2"
-	//u.Email = "test2@example.com"
-	//u.UpdateUser()
-	//u, _ = models.GetUser(1)
-	//fmt.Println(u)
+		u.Name = "test2"
+		u.Email = "test2@example.com"
+		u.UpdateUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
 
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+		u.DeleteUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
+
+	*/
 
 }
