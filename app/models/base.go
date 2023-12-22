ffa296ed15b92b003c719e1b9f1d891de1340e70
yosnak13 <yoshihisa.nakai@ft-labo.co.jp>
2023-12-22 15:51:37 +0900

    create todo

--- app/models/base.go
+++ app/models/base.go
@@ -17,6 +17,7 @@ var err error
 
 const (
 	tableNameUser = "users"
+	tableNameTodo = "todos"
 )
 
 func init() {
@@ -33,10 +34,15 @@ func init() {
     password STRING,
     created_at DATETIME)`, tableNameUser)
 
-	_, err = Db.Exec(cmdU)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	Db.Exec(cmdU)
+
+	cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user_id INTEGER,
+    created_at DATETIME)`, tableNameTodo)
+
+	Db.Exec(cmdT)
 }
 
 func createUUID() (uuidobj uuid.UUID) {
--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"todo_app/app/models"
+)
+
 func main() {
+	fmt.Println(models.Db)
 
-	/*
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@example.com"
-		u.PassWord = "testtest"
-		fmt.Println(u)
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
 
-		u.CreateUser()
-	*/
+	u.CreateUser()
 
 	/*
 		fmt.Println(models.Db)
@@ -28,5 +32,8 @@ func main() {
 		fmt.Println(u)
 
 	*/
+
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
 
 }
