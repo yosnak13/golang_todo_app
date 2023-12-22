25786c41b67e11d161f29a4589378e1adfc07386
yosnak13 <yoshihisa.nakai@ft-labo.co.jp>
2023-12-22 15:04:23 +0900

    get, update delete method for users

--- app/models/users.go
+++ app/models/users.go
@@ -29,3 +29,35 @@ func (u *User) CreateUser() (err error) {
 
 	return err
 }
+
+func GetUser(id int) (user User, err error) {
+	user = User{}
+	cmd := `select id, uuid, name, email, password, created_at from users where id = ?`
+	err = Db.QueryRow(cmd, id).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.PassWord,
+		&user.CreatedAt,
+	)
+	return user, err
+}
+
+func (u *User) UpdateUser() (err error) {
+	cmd := `update users set name = ?, email = ? where id = ?`
+	_, err = Db.Exec(cmd, u.Name, u.Email, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func (u *User) DeleteUser() (err error) {
+	cmd := `delete from users where id = ?`
+	_, err = Db.Exec(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
--- main.go
+++ main.go
@@ -6,11 +6,29 @@ import (
 )
 
 func main() {
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
+
+	/*
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@example.com"
+		u.PassWord = "testtest"
+		fmt.Println(u)
+
+		u.CreateUser()
+	*/
+
+	fmt.Println(models.Db)
+	u, _ := models.GetUser(1)
 	fmt.Println(u)
 
-	u.CreateUser()
+	//u.Name = "test2"
+	//u.Email = "test2@example.com"
+	//u.UpdateUser()
+	//u, _ = models.GetUser(1)
+	//fmt.Println(u)
+
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
 }
