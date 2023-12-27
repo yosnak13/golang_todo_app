f9a7275bdd16e786881068bcc57bf4414eb3be58
Yoshihisa Nakai <64535376+yosnak13@users.noreply.github.com>
2023-12-28 08:45:50 +0900

    Todo methods (#4)
    * create todo
    
    * get todo
    
    * Listtodos
    
    * ListTodoByUser
    
    * UpdateTodo
    
    * DeleteTodo
    
    ---------
    
    Co-authored-by: yosnak13 <yoshihisa.nakai@ft-labo.co.jp>

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"todo_app/app/models"
+)
+
 func main() {
+	fmt.Println(models.Db)
 
 	/*
 		u := &models.User{}
@@ -26,7 +32,51 @@ func main() {
 		u.DeleteUser()
 		u, _ = models.GetUser(1)
 		fmt.Println(u)
-
 	*/
 
+	/*
+		user, _ := models.GetUser(2)
+		user.CreateTodo("FirstTodo")
+	*/
+
+	/*
+		t, _ := models.GetTodo(1)
+		fmt.Println(t)
+	*/
+
+	/*
+		user, _ := models.GetUser(2)
+		user.CreateTodo("SecondTodo")
+
+		todos, _ := user.ListTodosByUser()
+		for _, v := range todos {
+			fmt.Println(v)
+		}
+	*/
+
+	/*
+		todos, _ := models.GetTodo()
+		for _, v := range todos {
+			fmt.Println(v)
+		}
+	*/
+
+	/*
+		t, _ := models.GetTodo(1)
+		fmt.Println("-------------------")
+		fmt.Println(t)
+		t.Content = "Update Todo"
+		t.UpdateTodo()
+
+		updatedTodo, _ := models.GetTodo(1)
+		fmt.Println("-------------------")
+		fmt.Println(updatedTodo)
+	*/
+
+	/*
+		t, _ := models.GetTodo(3)
+		t.DeleteTodo()
+		deletedTodo, _ := models.GetTodo(3)
+		fmt.Println(deletedTodo)
+	*/
 }
