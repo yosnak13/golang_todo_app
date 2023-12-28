70e829040a4059be801fbe993ff2650c52f72ff0
Yoshihisa Nakai <64535376+yosnak13@users.noreply.github.com>
2023-12-28 09:06:36 +0900

    create default server and handler (#5)
    Co-authored-by: yosnak13 <yoshihisa.nakai@ft-labo.co.jp>

--- main.go
+++ main.go
@@ -2,81 +2,12 @@ package main
 
 import (
 	"fmt"
+	"todo_app/app/controllers"
 	"todo_app/app/models"
 )
 
 func main() {
 	fmt.Println(models.Db)
 
-	/*
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@example.com"
-		u.PassWord = "testtest"
-		fmt.Println(u)
-
-		u.CreateUser()
-	*/
-
-	/*
-		fmt.Println(models.Db)
-		u, _ := models.GetUser(1)
-		fmt.Println(u)
-
-		u.Name = "test2"
-		u.Email = "test2@example.com"
-		u.UpdateUser()
-		u, _ = models.GetUser(1)
-		fmt.Println(u)
-
-		u.DeleteUser()
-		u, _ = models.GetUser(1)
-		fmt.Println(u)
-	*/
-
-	/*
-		user, _ := models.GetUser(2)
-		user.CreateTodo("FirstTodo")
-	*/
-
-	/*
-		t, _ := models.GetTodo(1)
-		fmt.Println(t)
-	*/
-
-	/*
-		user, _ := models.GetUser(2)
-		user.CreateTodo("SecondTodo")
-
-		todos, _ := user.ListTodosByUser()
-		for _, v := range todos {
-			fmt.Println(v)
-		}
-	*/
-
-	/*
-		todos, _ := models.GetTodo()
-		for _, v := range todos {
-			fmt.Println(v)
-		}
-	*/
-
-	/*
-		t, _ := models.GetTodo(1)
-		fmt.Println("-------------------")
-		fmt.Println(t)
-		t.Content = "Update Todo"
-		t.UpdateTodo()
-
-		updatedTodo, _ := models.GetTodo(1)
-		fmt.Println("-------------------")
-		fmt.Println(updatedTodo)
-	*/
-
-	/*
-		t, _ := models.GetTodo(3)
-		t.DeleteTodo()
-		deletedTodo, _ := models.GetTodo(3)
-		fmt.Println(deletedTodo)
-	*/
+	controllers.StartMainServer()
 }
