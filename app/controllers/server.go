ee9b71883fded3393e700d15ee0ae34ae768fe8d
Yoshihisa Nakai <64535376+yosnak13@users.noreply.github.com>
2023-12-28 15:28:30 +0900

    Bootstrap (#6)
    * import bootstrap&jquery component
    
    * staticファイル設定
    
    * layout作成
    
    ---------
    
    Co-authored-by: yosnak13 <yoshihisa.nakai@ft-labo.co.jp>

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,11 +1,28 @@
 package controllers
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 	"todo_app/config"
 )
 
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+
+}
+
 func StartMainServer() error {
+
+	// Static読み込み。"/static/"ディレクトリはないためStripPrefixで取り除く
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
 
 	// URL登録
 	http.HandleFunc("/", top)
--- config/config.go
+++ config/config.go
@@ -11,6 +11,7 @@ type AppConfigList struct {
 	SQLDriver string
 	DbName    string
 	LogFile   string
+	Static    string
 }
 
 var Config AppConfigList
@@ -30,5 +31,6 @@ func LoadConfig() {
 		SQLDriver: cfg.Section("db").Key("driver").String(),
 		DbName:    cfg.Section("db").Key("name").String(),
 		LogFile:   cfg.Section("web").Key("logfile").String(),
+		Static:    cfg.Section("web").Key("static").String(),
 	}
 }
