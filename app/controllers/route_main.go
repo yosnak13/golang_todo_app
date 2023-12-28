e8b362bf5d9c43e8dd3500b0296f77ed9f13ac3d
yosnak13 <yoshihisa.nakai@ft-labo.co.jp>
2023-12-28 15:27:19 +0900

    layout作成

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,9 @@
 package controllers
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/templates/top.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	t.Execute(w, nil)
+	generateHTML(w, "Hello", "layout", "top")
 }
