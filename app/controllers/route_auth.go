a67f282631d31375072c0b8bfdcfa60d6457ce32
yosnak13 <yoshihisa.nakai@ft-labo.co.jp>
2023-12-29 10:27:56 +0900

    login

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -28,3 +28,34 @@ func signup(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", 302)
 	}
 }
+
+func login(w http.ResponseWriter, r *http.Request) {
+	generateHTML(w, nil, "layout", "public_navbar", "login")
+}
+
+func authenticate(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	user, err := models.GetUserByEmail(r.PostFormValue("email"))
+	if err != nil {
+		log.Println(err)
+		http.Redirect(w, r, "/login", 302)
+	}
+	if user.PassWord == models.Encrypt(r.PostFormValue("password")) {
+		session, err := user.CreateSession()
+		if err != nil {
+			log.Println(err)
+		}
+
+		cookie := http.Cookie{
+			Name:     "_cookie",
+			Value:    session.UUID,
+			HttpOnly: true,
+		}
+		http.SetCookie(w, &cookie)
+
+		http.Redirect(w, r, "/top", 302)
+	} else {
+		// Password誤り
+		http.Redirect(w, r, "/login", 302)
+	}
+}
--- app/models/users.go
+++ app/models/users.go
@@ -14,6 +14,14 @@ type User struct {
 	CreatedAt time.Time
 }
 
+type Session struct {
+	Id        int
+	UUID      string
+	Email     string
+	UserId    string
+	CreatedAt string
+}
+
 func (u *User) CreateUser() (err error) {
 	cmd := `insert into users (
                    uuid,
@@ -60,4 +68,58 @@ func (u *User) DeleteUser() (err error) {
 		log.Fatalln(err)
 	}
 	return err
+}
+
+func GetUserByEmail(email string) (user User, err error) {
+	user = User{}
+	cmd := `select id, uuid, name, email, password, created_at from users where email = ?`
+	err = Db.QueryRow(cmd, email).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.PassWord,
+		&user.CreatedAt)
+
+	return user, err
+}
+
+func (u *User) CreateSession() (session Session, err error) {
+	session = Session{}
+	insert := `insert into sessions (uuid, email, user_id, created_at) values (?, ?, ?, ?)`
+
+	_, err = Db.Exec(insert, createUUID(), u.Email, u.ID, time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	userSession := `select id, uuid, email, user_id, created_at from sessions where user_id = ?`
+	err = Db.QueryRow(userSession, u.ID, u.Email).Scan(
+		&session.Id,
+		&session.UUID,
+		&session.Email,
+		&session.UserId,
+		&session.CreatedAt)
+
+	return session, err
+}
+
+func (sess *Session) CheckSession() (valid bool, err error) {
+	cmd := `select id, uuid, email, user_id, created_at from sessions where uuid = ?`
+
+	err = Db.QueryRow(cmd, sess.UUID).Scan(
+		&sess.Id,
+		&sess.UUID,
+		&sess.Email,
+		&sess.UserId,
+		&sess.CreatedAt)
+
+	if err != nil {
+		valid = false
+		return
+	}
+	if sess.Id != 0 {
+		valid = true
+	}
+	return valid, err
 }
