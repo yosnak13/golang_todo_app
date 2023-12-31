6dcd6f70c4ad35916b1084f583e081f8845f1146
yosnak13 <yosnak13@gmail.com>
2023-12-31 09:47:20 +0900

    create todo function

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -30,3 +30,35 @@ func index(w http.ResponseWriter, r *http.Request) {
 		generateHTML(w, user, "layout", "private_navbar", "index")
 	}
 }
+
+func todoNew(w http.ResponseWriter, r *http.Request) {
+	_, err := getCookie(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		generateHTML(w, nil, "layout", "private_navbar", "todo_new")
+	}
+}
+
+func todoSave(w http.ResponseWriter, r *http.Request) {
+	sess, err := getCookie(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err = r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		content := r.PostFormValue("content")
+		if err := user.CreateTodo(content); err != nil {
+			log.Println(err)
+		}
+
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
